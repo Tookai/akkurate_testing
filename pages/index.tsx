3f2ec55b2020ec70d4d748e45a1b920e04b6f3f4
vincent crosnier <92791698+vincentpresentation12@users.noreply.github.com>
2022-12-23 11:34:36 +0100

    test push vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import { AcsInputText } from '@akkurateio/ds'
-import { Box, HStack, Text, VStack } from '@chakra-ui/react'
+import {AcsInputPhone, AcsInputText} from '@akkurateio/ds'
+import {Box, HStack, SimpleGrid, Text, VStack} from '@chakra-ui/react'
 import type { NextPage } from 'next'
 import { useState } from 'react'
 import ToggleColorMode from '../components/atoms/ToggleColorMode'
 
 const Home: NextPage = () => {
 	const [inputText, setInputText] = useState<string>('')
+	const [inputPhone, setInputPhone] = useState<string>('')
 
 	return (
 		<Box width={'100vw'} px={8} py={2}>
@@ -14,16 +15,17 @@ const Home: NextPage = () => {
 
 				<ToggleColorMode />
 			</HStack>
+			<Text>Test des composants</Text>
 
-			<VStack>
-				<Text>Test des composants</Text>
+			<SimpleGrid columns={3} spacing={10}>
 
 				<AcsInputText
 					label={'test'}
 					value={inputText}
 					handleChange={setInputText}
 				/>
-			</VStack>
+				<AcsInputPhone label={'phone'} handleChange={setInputPhone} value={inputPhone} />
+			</SimpleGrid>
 		</Box>
 	)
 }
