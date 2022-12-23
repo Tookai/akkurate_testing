aa14c6d7fefbe7216c3e83fccbeaf911a4bf83f7
Thibaut Orcel <71818808+Tookai@users.noreply.github.com>
2022-12-23 11:26:37 +0100

    config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
-import { Box, Text } from '@chakra-ui/react'
+import { AcsInputText } from '@akkurateio/ds'
+import { Box, HStack, Text, VStack } from '@chakra-ui/react'
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import ToggleColorMode from '../components/atoms/ToggleColorMode'
 
 const Home: NextPage = () => {
-	return (
-		<Box width={'100vw'} height={'100vh'}>
-			<Text fontSize={'3xl'}>hello world</Text>
+	const [inputText, setInputText] = useState<string>('')
 
-			<ToggleColorMode />
+	return (
+		<Box width={'100vw'} px={8} py={2}>
+			<HStack width={'full'} justifyContent={'space-between'}>
+				<Text fontSize={'3xl'}>hello world</Text>
+
+				<ToggleColorMode />
+			</HStack>
+
+			<VStack>
+				<Text>Test des composants</Text>
+
+				<AcsInputText
+					label={'test'}
+					value={inputText}
+					handleChange={setInputText}
+				/>
+			</VStack>
 		</Box>
 	)
 }
